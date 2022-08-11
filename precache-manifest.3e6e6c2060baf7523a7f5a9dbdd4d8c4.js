9958b48be5eb87ace4c825fbfd1f38a9fe4e86d3
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-11 17:28:30 +0600

    Updates

---
+++ precache-manifest.3e6e6c2060baf7523a7f5a9dbdd4d8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc0c2ec643da071f77f5f5a106016be",
+    "url": "/react_text_generator/index.html"
+  },
+  {
+    "revision": "2c83c380cc72722ac79e",
+    "url": "/react_text_generator/static/css/main.571b8f7a.chunk.css"
+  },
+  {
+    "revision": "fe4d6052aa6eb82a67f9",
+    "url": "/react_text_generator/static/js/2.9be38692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_text_generator/static/js/2.9be38692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c83c380cc72722ac79e",
+    "url": "/react_text_generator/static/js/main.d055aa06.chunk.js"
+  },
+  {
+    "revision": "51ae453aace22d9f2c93",
+    "url": "/react_text_generator/static/js/runtime-main.9964a238.js"
+  }
+]);
